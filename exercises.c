6ecda6ee98028b5ac47350af9396ebf0249184dd
Your Name <you@example.com>
2025-03-23 00:58:54 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -129,8 +129,8 @@ int parentesisBalanceados(char *cadena) {
     }
     else{
       if(cadena[cont] == '('&& *(char*)top(PAper) == ')' ||  
-        cadena[cont] == '{'&& top(PAper) == '}' || 
-        cadena[cont] == '['&& top(PAper) == ']' ){
+        cadena[cont] == '{'&& *(char*)top(PAper) == '}' || 
+        cadena[cont] == '['&& *(char*)top(PAper) == ']' ){
         ;
       }
     }
